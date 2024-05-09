26872d6de1273e34fba6aad87774cf75c0f609c3
reinheimermat <mreinheimer0@gmail.com>
2024-05-09 20:55:37 -0300

    feat: finish card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,6 +1,7 @@
-import { ShoppingCartSimple } from '@phosphor-icons/react'
+import { CheckFat, ShoppingCartSimple } from '@phosphor-icons/react'
 import { QuantityInput } from './quantityInput'
 import { useState } from 'react'
+import { useCart } from '../hooks/useCart'
 
 interface CardProps {
   coffee: {
@@ -15,6 +16,8 @@ interface CardProps {
 
 export function Card({ coffee }: CardProps) {
   const [quantity, setQuantity] = useState(1)
+  const [isItemAdded, setIsItemAdded] = useState(false)
+  const { addItem } = useCart()
 
   function incrementQuantity() {
     setQuantity((state) => state + 1)
@@ -26,14 +29,20 @@ export function Card({ coffee }: CardProps) {
     }
   }
 
+  function handleAddItem() {
+    addItem({ id: coffee.id, quantity })
+    setIsItemAdded(true)
+    setQuantity(1)
+  }
+
   return (
-    <div className="bg-baseCard flex max-w-64 flex-col items-center justify-between rounded-bl-[2.25rem] rounded-br-md rounded-tl-md rounded-tr-[2.25rem] px-6 pb-5 text-center">
+    <div className="flex max-w-64 flex-col items-center justify-between rounded-bl-[2.25rem] rounded-br-md rounded-tl-md rounded-tr-[2.25rem] bg-baseCard px-6 pb-5 text-center">
       <img className="-mt-5 mb-4" src={coffee.image} alt={coffee.title} />
 
       <div>
         {coffee.tags.map((tag) => (
           <span
-            className="bg-yellowLight text-yellowDark me-1 rounded-full p-2 text-xs font-bold uppercase"
+            className="me-1 rounded-full bg-yellowLight p-2 text-xs font-bold uppercase text-yellowDark"
             key={tag}
           >
             {tag}
@@ -41,16 +50,16 @@ export function Card({ coffee }: CardProps) {
         ))}
       </div>
 
-      <h1 className="text-baseSubtitle font-title mb-2 mt-4 text-xl font-bold">
+      <h1 className="mb-2 mt-4 font-title text-xl font-bold text-baseSubtitle">
         {coffee.title}
       </h1>
 
-      <h2 className="text-baseLabel mb-8 text-sm">{coffee.description}</h2>
+      <h2 className="mb-8 text-sm text-baseLabel">{coffee.description}</h2>
 
       <div className="flex w-full items-center justify-between">
         <div>
-          <span className="text-baseText me-1 text-sm">R$</span>
-          <span className="font-title text-baseText text-2xl font-extrabold">
+          <span className="me-1 text-sm text-baseText">R$</span>
+          <span className="font-title text-2xl font-extrabold text-baseText">
             {coffee.price.toFixed()}
           </span>
         </div>
@@ -62,11 +71,19 @@ export function Card({ coffee }: CardProps) {
             decrementQuantity={decrementQuantity}
           />
 
-          <button className="bg-purpleDark hover:bg-purple rounded-md p-2">
-            <ShoppingCartSimple
-              weight="fill"
-              className="text-baseCard text-2xl"
-            />
+          <button
+            className="rounded-md bg-purpleDark p-2 hover:bg-purple"
+            onClick={handleAddItem}
+            disabled={isItemAdded}
+          >
+            {isItemAdded ? (
+              <CheckFat className="text-2xl text-baseCard" weight="fill" />
+            ) : (
+              <ShoppingCartSimple
+                weight="fill"
+                className="text-2xl text-baseCard"
+              />
+            )}
           </button>
         </div>
       </div>

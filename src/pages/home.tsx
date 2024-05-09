666579d5303fe84704ad15ea900e2f507a19a9db
reinheimermat <mreinheimer0@gmail.com>
2024-05-09 20:59:47 -0300

    feat: add styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,15 +5,15 @@ import { Card } from '../components/card'
 export function Home() {
   return (
     <main>
-      <section className="bg-image h-[34rem] bg-cover bg-no-repeat object-cover">
+      <section className="h-[34rem] bg-image bg-cover bg-no-repeat object-cover">
         <div className="container mx-auto max-w-[70rem]">
           <div className="flex items-center justify-between pt-24">
             <article className="max-w-[36.75rem]">
               <div className="mb-16">
-                <h1 className="text-baseTitle font-title mb-4 text-5xl font-extrabold">
+                <h1 className="mb-4 font-title text-5xl font-extrabold text-baseTitle">
                   Encontre o café perfeito para qualquer hora do dia
                 </h1>
-                <span className="text-baseSubtitle text-xl">
+                <span className="text-xl text-baseSubtitle">
                   Com o Coffee Delivery você recebe seu café onde estiver, a
                   qualquer hora
                 </span>
@@ -23,14 +23,14 @@ export function Home() {
                   <ShoppingCart
                     size={32}
                     weight="fill"
-                    className="text-background bg-yellowDark size-8 rounded-full p-2"
+                    className="size-8 rounded-full bg-yellowDark p-2 text-background"
                   />
                   <span className="text-baseText">Compra simples e segura</span>
                 </div>
                 <div className="col-span-1 flex items-center gap-3">
                   <Package
                     weight="fill"
-                    className="text-background bg-baseText size-8 rounded-full p-2"
+                    className="size-8 rounded-full bg-baseText p-2 text-background"
                   />
                   <span className="text-baseText">
                     Embalagem mantém o café intacto
@@ -39,7 +39,7 @@ export function Home() {
                 <div className="col-span-1 flex items-center gap-3">
                   <Timer
                     weight="fill"
-                    className="text-background bg-yellow size-8 rounded-full p-2"
+                    className="size-8 rounded-full bg-yellow p-2 text-background"
                   />
                   <span className="text-baseText">
                     Entrega rápida e rastreada
@@ -48,7 +48,7 @@ export function Home() {
                 <div className="col-span-1 flex items-center gap-3">
                   <Coffee
                     weight="fill"
-                    className="text-background bg-purple size-8 rounded-full p-2"
+                    className="size-8 rounded-full bg-purple p-2 text-background"
                   />
                   <span className="text-baseText">
                     O café chega fresquinho até você
@@ -68,7 +68,7 @@ export function Home() {
       </section>
 
       <section className="container mx-auto max-w-[70rem]">
-        <h2 className="font-title text-baseSubtitle my-9 text-4xl font-extrabold">
+        <h2 className="my-9 font-title text-4xl font-extrabold text-baseSubtitle">
           Nossos cafés
         </h2>
 

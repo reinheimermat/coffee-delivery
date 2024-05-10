ce5f4dcc015e3991bb3321c228b09a312c19d83d
reinheimermat <mreinheimer0@gmail.com>
2024-05-09 21:00:39 -0300

    feat: add router to pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Home } from './pages/home.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Cart } from './pages/cart.tsx'
+import { Success } from './pages/success.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/:orderId/success', element: <Success /> },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
